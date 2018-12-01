ec33d46a9c6440b448d588201dd92c448f1a335c
James Neilson <james@quandl.com>
2018-11-30 19:07:45 -0500

    Write method to return possible class combinations

--- generator.rb
+++ generator.rb
@@ -1,13 +1,44 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'facets'
 
 # Picks a class for a player to play
 class Generator
-  attr_accessor :class_json
+  attr_accessor :classes, :attributes, :class_data
 
   def initialize
-    file = File.read('classes.json')
-    @class_json = JSON.parse(file)
+    @classes = class_json.keys
+    @attributes = find_attributes
+    @class_data = class_json
+  end
+
+  def possible_classes(traits)
+    classes = {}
+
+    class_data.each do |class_name, attributes|
+      classes[class_name] = 0
+
+      traits.each do |trait|
+        classes[class_name] += 1 if attributes.include?(trait)
+      end
+    end
+    classes.reject { |name| classes[name].zero? }
+  end
+
+  private
+
+  def class_json
+    JSON.parse(File.read('classes.json'))
+  end
+
+  def find_attributes
+    attributes = []
+    class_json.each do |_class_name, attribute_list|
+      attribute_list.each do |attribute|
+        attributes << attribute unless attributes.include?(attribute)
+      end
+    end
+    attributes
   end
 end
